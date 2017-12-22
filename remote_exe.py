8220d6b3182a9829d81d81190eb5e0c147d235c1
stevengao1028 <63297269@qq.com>
2017-12-22 11:19:14 +0800

    remote put command

--- remote_exe.py
+++ remote_exe.py
@@ -15,5 +15,16 @@ def remote_exec(ip,username,passwd,exe_command):
     s.close()
     return result
 
-
-
+def remote_put(ip,username,passwd,local_path,remote_path):
+    try:
+        t = paramiko.Transport((ip, 22))
+        t.connect(username=username, password=passwd)
+        sftp = paramiko.SFTPClient.from_transport(t)
+        sftp.put(local_path, remote_path)
+        t.close()
+    except Exception, e:
+        result = {'status': "1", 'info': "push fault"}
+    else:
+        result = {'status': "0", 'info': "sucessful"}
+    t.close()
+    return result
