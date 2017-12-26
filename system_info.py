872d6d8f8c22d366b6635db8c08871a6bec3ebb8
steven <63297269@qq.com>
2017-12-26 21:55:28 +0800

    zpool info

--- system_info.py
+++ system_info.py
@@ -5,10 +5,10 @@ import re
 
 def disk_info(ip="127.0.0.1",disk=""):
     #获取系统blk 磁盘信息 ，包含名称，大小，用途的磁盘列表
-    exe_cmd = """lsscsi --size |awk '$2~/disk/{print $6,$7}'"""
+    exe_cmd = """lsscsi -ws |awk '$2~/disk/{print $3,$4}'"""
     exe_result = exe_command(ip,exe_cmd)
     disk_list = []
-    if exe_result['status'] == "0":
+    if exe_result['status'] == "0" and exe_result['info']:
         for disk in exe_result['info'].split('\n'):
             disk_info = {'name': "", 'size': "", 'usage': "",'type':""}
             disk_info['name'] = disk.split()[0].split('/dev/')[1]
@@ -34,13 +34,13 @@ def zpool_info(ip="127.0.0.1",pool_name=""):
     exe_cmd_pool = "zpool status "+pool_name
     exe_result_pool = exe_command(ip,exe_cmd_pool)
     pool_list =[]
-    exe_cmd_zfs = "zfs list |awk '$1~/\//{print $1,$2,$3}'"
+    exe_cmd_zfs = "zfs list -o name,logicalused,volsize,type,used"
     exe_result_zfs = exe_command(ip, exe_cmd_zfs)
     if exe_result_pool['status'] != "0" or exe_result_zfs['status'] != "0" :
         result = {'status': "1", 'info': "lookup fails"}
         return result
     for pool in exe_result_pool['info'].split('errors'):
-        pool_info = {'name':'','state':'','disks':[],'spares':[],'raid':'0','volumes':[],'used':'','free':''}
+        pool_info = {'name':'','state':'','disks':[],'spares':[],'raid':'0','volumes':[],'used':'','size':''}
         disks=[]
         spares=[]
         volumes=[]
@@ -62,15 +62,18 @@ def zpool_info(ip="127.0.0.1",pool_name=""):
                     pool_info['raid'] = "5"
                 if  item.startswith('raidz2'):
                     pool_info['raid'] = "6"
-                vol_info ={}
-                for vol in exe_result_zfs['info'].split('\n'):
+        if exe_result_zfs['info']:
+            for vol in exe_result_zfs['info'].split('\n'):
+                if  "filesystem" in vol.split()[3]:
+                    pool_info['used'] = vol.split()[4]
+                elif "volume" in vol.split()[3]:
                     if pool_info['name'] == vol.split()[0].split('/')[0]:
                         vol_name = vol.split()[0].split('/')[1]
                         vol_used = vol.split()[1]
-                        vol_free = vol.split()[2]
-                        vol_info = {'vol_name':vol_name,'vol_used':vol_used,'vol_free':vol_free}
+                        vol_size = vol.split()[2]
+                        vol_info = {'vol_name':vol_name,'vol_used':vol_used,'vol_size':vol_size}
                         volumes.append(vol_info)
-                pool_info['volumes'] = volumes
+            pool_info['volumes'] = volumes
 
         if pool_info['name']:
             pool_list.append(pool_info)
